6d974e1d98089dd3044f8ba2719395607b5786ef
Dmitri Shuralyov <shurcooL@gmail.com>
2016-07-09 20:03:16 -0700

    Sync license with upstream
    Change copyright in license to 2009, that's what the latest upstream
    LICENSE now has (see https://github.com/golang/go/commit/d0c1888739ac0d5d0c9f82a4b86945c0351caef6).

--- match.go
+++ match.go
@@ -2,7 +2,7 @@ package gotool
 
 // This file contains code from the Go distribution.
 
-// Copyright (c) 2012 The Go Authors. All rights reserved.
+// Copyright (c) 2009 The Go Authors. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
